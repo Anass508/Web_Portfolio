72a2dff6601171b08e89a23982ac7d2e9052be41
Anass Fathi <anass78fathi1@gmail.com>
2026-09-06 15:05:58 +0200

    Implement burger menu animation
    Added functionality for burger menu animation on button click.

--- v.2 - Web_Portfolio/script.js
+++ v.2 - Web_Portfolio/script.js
@@ -1,1 +1,10 @@
 console.log("ok");
+
+
+/* Animation du burger-menu */
+
+let btn=document.querySelector('.btn-menu');
+
+btn.addEventListener('click', function (){
+    btn.classList.toggle('active'); /* toggle sert  a ajouter ou enlever la class active lorque que l'on clique dessus */
+});
