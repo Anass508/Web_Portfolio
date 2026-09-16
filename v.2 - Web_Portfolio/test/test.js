3803cfe12b2d2c1473cac5c98c0f7bda8164f674
Anass Fathi <anass78fathi1@gmail.com>
2026-09-16 23:06:32 +0200

    Refactor card component for improved mouse interaction

--- v.2 - Web_Portfolio/test/test.js
+++ v.2 - Web_Portfolio/test/test.js
@@ -1,77 +1,50 @@
-Vue.config.devtools = true;
+document.addEventListener('DOMContentLoaded', () => {
+  const cardWraps = document.querySelectorAll('.carte-wrap');
 
-Vue.component('card', {
-  template: `
-    <div class="card-wrap"
-      @mousemove="handleMouseMove"
-      @mouseenter="handleMouseEnter"
-      @mouseleave="handleMouseLeave"
-      ref="card">
-      <div class="card"
-        :style="cardStyle">
-        <div class="card-bg" :style="[cardBgTransform, cardBgImage]"></div>
-        <div class="card-info">
-          <slot name="header"></slot>
-          <slot name="content"></slot>
-        </div>
-      </div>
-    </div>`,
-  mounted() {
-    this.width = this.$refs.card.offsetWidth;
-    this.height = this.$refs.card.offsetHeight;
-  },
-  props: ['dataImage'],
-  data: () => ({
-    width: 0,
-    height: 0,
-    mouseX: 0,
-    mouseY: 0,
-    mouseLeaveDelay: null
-  }),
-  computed: {
-    mousePX() {
-      return this.mouseX / this.width;
-    },
-    mousePY() {
-      return this.mouseY / this.height;
-    },
-    cardStyle() {
-      const rX = this.mousePX * 30;
-      const rY = this.mousePY * -30;
-      return {
-        transform: `rotateY(${rX}deg) rotateX(${rY}deg)`
-      };
-    },
-    cardBgTransform() {
-      const tX = this.mousePX * -40;
-      const tY = this.mousePY * -40;
-      return {
-        transform: `translateX(${tX}px) translateY(${tY}px)`
-      }
-    },
-    cardBgImage() {
-      return {
-        backgroundImage: `url(${this.dataImage})`
-      }
+  cardWraps.forEach((cardWrap) => {
+    const card = cardWrap.querySelector('.carte');
+    const cardBg = cardWrap.querySelector('.carte-bg'); 
+    
+    
+    const imageUrl = cardWrap.getAttribute('data-image');
+    if (imageUrl) {
+      cardBg.style.backgroundImage = `url(${imageUrl})`;
     }
-  },
-  methods: {
-    handleMouseMove(e) {
-      this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width/2;
-      this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height/2;
-    },
-    handleMouseEnter() {
-      clearTimeout(this.mouseLeaveDelay);
-    },
-    handleMouseLeave() {
-      this.mouseLeaveDelay = setTimeout(()=>{
-        this.mouseX = 0;
-        this.mouseY = 0;
+
+    let mouseLeaveTimeout;
+
+    cardWrap.addEventListener('mousemove', (e) => {
+      const width = cardWrap.offsetWidth;
+      const height = cardWrap.offsetHeight;
+      
+      
+      const rect = cardWrap.getBoundingClientRect();
+      const mouseX = e.clientX - rect.left - width / 2;
+      const mouseY = e.clientY - rect.top - height / 2;
+
+      const mousePX = mouseX / width;
+      const mousePY = mouseY / height;
+
+      
+      const rX = mousePX * 30;
+      const rY = mousePY * -30;
+      card.style.transform = `rotateY(${rX}deg) rotateX(${rY}deg)`;
+      
+      
+      const tX = mousePX * -40;
+      const tY = mousePY * -40;
+      cardBg.style.transform = `translateX(${tX}px) translateY(${tY}px)`;
+    });
+
+    cardWrap.addEventListener('mouseenter', () => {
+      clearTimeout(mouseLeaveTimeout);
+    });
+
+    cardWrap.addEventListener('mouseleave', () => {
+      mouseLeaveTimeout = setTimeout(() => {
+        card.style.transform = 'rotateY(0deg) rotateX(0deg)';
+        cardBg.style.transform = 'translateX(0px) translateY(0px)';
       }, 1000);
-    }
-  }
-});
-
-const app = new Vue({
-  el: '#app'
+    });
+  });
 });
